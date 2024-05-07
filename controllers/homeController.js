375105deac124e050c1eb696876cabdf5415c927
Chae seoyeon <chaesy02@gmail.com>
2024-05-07 11:39:52 +0000

    [feat] home.ejs 렌더링하도록 수정

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,6 @@
 exports.getHomePage = async (req, res) => {
     try {
-        res.send('Welcome to Yorijori');
+        res.render('home'); // home.ejs 파일 렌더링(메인페이지 화면)
     } catch (err) {
         console.error("Error loading the home page:", err);
         res.status(500).send({
