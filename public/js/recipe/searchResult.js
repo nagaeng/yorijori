4b245ea1b08634a874686ff1842e138371e3f103
Chae seoyeon <chaesy02@gmail.com>
2024-05-26 16:38:19 +0000

    [feat] 검색결과 프론트 수정

--- public/js/recipe/searchResult.js
+++ public/js/recipe/searchResult.js
@@ -1,1 +1,23 @@
-// 선택한 정렬 카테고리 스타일 변경
+document.addEventListener("DOMContentLoaded", () => {
+  const nonClick = document.querySelectorAll(".sort-method");
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const sortParam = urlParams.get('sort') || 'popularity';
+
+  const activeElement = document.getElementById(sortParam);
+  if (activeElement) {
+    activeElement.classList.add("click");
+  }
+  function handleClick(event) {
+    // div에서 모든 "click" 클래스 제거
+    nonClick.forEach((e) => {
+      e.classList.remove("click");
+    });
+    // 클릭한 div만 "click"클래스 추가
+    event.target.classList.add("click");
+  }
+
+  nonClick.forEach((e) => {
+    e.addEventListener("click", handleClick);
+  });
+});
