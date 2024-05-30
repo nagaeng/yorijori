52d910d6d4af02c46fccd0c605ec6880e9dc1333
nagaeng <helen030816@naver.com>
2024-05-30 06:45:34 +0000

    refactor: 복구 3. createFunding 관련 라우터, 컨트롤러 수정

--- routers/createFundingRouter.js
+++ routers/createFundingRouter.js
@@ -3,10 +3,10 @@ const router = express.Router();
 db = require("../models/index");
 const createFundingController = require("../controllers/createFundingController.js");
 
-router.get('/create_funding', createFundingController.showCreateFundingPage);
 router.get('/create_funding_search', createFundingController.showCreateFundingSearchPage);
-router.get('/create_funding/:productId', createFundingController.searchProducts);
-router.get('/create_funding/:productId/creating', createFundingController.createFunding);
-router.post('/create_funding_success/:productId/success', createFundingController.successFunding);
+router.get('/search_results', createFundingController.searchProducts);
+router.get('/product_detail/:productId', createFundingController.productDetail);
+router.get('/create_funding/:productId', createFundingController.showCreateFundingPage);
+router.post('/create_funding', createFundingController.createFunding);
 
 module.exports = router;
