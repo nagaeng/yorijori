03d0d58a7092fd9b78dec5730ed21ca60f3b4c64
limyeeun <20211146@sungshin.ac.kr>
2024-05-31 07:29:11 +0000

    로그인 유저정보로 변경, css 수정, 로그인화면 수정

--- controllers/joinFundingController.js
+++ controllers/joinFundingController.js
@@ -1,6 +1,3 @@
-// // const fundingGroup = require("../models/fundingGroup"); //뭐냐얘?왜있더라
-// const Sequelize = require('sequelize');;
-
 const db = require("../models/index"),
     FundingProduct = db.fundingProduct,
     FundingGroup = db.fundingGroup,
@@ -8,11 +5,7 @@ const db = require("../models/index"),
     Composition = db.composition,
     sequelize = db.sequelize,
     Sequelize = db.Sequelize;
-// getDataParams = body =>{
-//     return {
 
-//     }
-// }
 getJuso = group => { //주소합치기
     let juso = `${group.city} ${group.district} ${group.town} ${group.detail}`;
     return juso;
@@ -20,13 +13,6 @@ getJuso = group => { //주소합치기
 module.exports = {
     fundingList: async (req, res, next) => {
         try {
-            //  let query2 = `
-            //                     SELECT COUNT(*)
-            //                     FROM compositions
-            //                     WHERE compositions.fundingGroupId = 2;`
-            // let [results, metadata] = await sequelize.query(query2, { type: Sequelize.SELECT });
-            // let peopleCount = results[0];
-            // let currentUser = res.locals.currentUser;
             //findAll()로 했더니 원하는 결과가 안나와서 raw 쿼리 사용함. 펀딩그룹을 기준으로 펀딩상품과 유저 테이블을 조인해서 정보가져옴.
             let userId = res.locals.currentUser.getDataValue('userId');
             let query = `
@@ -54,7 +40,6 @@ module.exports = {
                                                     WHERE compositions.fundingGroupId = fundingGroups.fundingGroupId);`
                             //배부장소와 구매자의 주소가 일치
                             //펀딩인원이 다 차면 목록에 나오지않게
-            //유저 id 로그인한 유저의 id로 수정필요 1 => currentUser.userId or currentUser.getDataValue('userId')
             let products = await sequelize.query(query, { type: Sequelize.SELECT });
             res.locals.products = products;
             next();
@@ -64,22 +49,12 @@ module.exports = {
         }
     },
     getFundingPage: async (req, res) => { //펀딩그룹모집중인 목록 보여주는 처음페이지
-
-
-        // let query2 = `                        
-        //             select count(fundingGroupId) as c
-        //             from compositions
-        //             where fundingGroupId = ${groups.fundingGroupId}; 
-        //             `
-        // let [results, metadata] = await sequelize.query(query2, { type: Sequelize.SELECT });
-        // let people = groups.people - results[0].c -1 ; //전체펀딩인원 - 펀딩참여인원 - 펀딩대표 = 남은 인원
         let products = res.locals.products;
         res.render('funding/fundingPage', { products: products[0], messages: req.flash('info') }); //products안에 동일한 객체 2개가 배열로 이루어져있어서 첫번째 객체만 출력하게함.
 
     },
     fundingSearch: async (req, res, next) => {
         try {
-            // let currentUser = res.locals.currentUser;
             let userId = res.locals.currentUser.getDataValue('userId');
             let query = req.query.query;
             let sql = `SELECT fundingGroups.fundingGroupId,
@@ -105,7 +80,6 @@ module.exports = {
                             WHERE compositions.fundingGroupId = fundingGroups.fundingGroupId) AND productName LIKE ?`;
                             //배부장소와 구매자의 주소가 일치
                             //펀딩인원이 다 차면 목록에 나오지않게
-                            //유저 id 로그인한 유저의 id로 수정필요 1 => currentUser.userId or currentUser.getDataValue('userId')
             let values
             if (!query) { //검색창에 아무것도 입력하지 않았을 때
                 res.redirect('/joinfundingPage/fundingPage');
@@ -161,11 +135,9 @@ module.exports = {
                             WHERE fundingGroups.fundingGroupId = ${groupId};`
             let result = await sequelize.query(query, { type: Sequelize.SELECT });
             res.locals.group = result[0]; //result에 [[{키:값}],[{키:값}]]형태로 동일한 객체가 두개 존재해서 첫번째[{}]만 추출.
-            // res.locals.groupId = groupId;
-            // console.log(res.locals.group);
-            let price = res.locals.group[0].unitPrice + (res.locals.group[0].deliveryCost / (res.locals.group[0].people - 1));
+        
+            let price = res.locals.group[0].unitPrice + (res.locals.group[0].deliveryCost / (res.locals.group[0].people - 1)); //결제금액 계산
             res.locals.price = price;
-            // console.log(res.locals.price);
             next();
         } catch (error) {
             res.status(500).send({ message: error.message });
@@ -183,15 +155,10 @@ module.exports = {
                     `
         let [results, metadata] = await sequelize.query(query, { type: Sequelize.SELECT });
         let people = groups.people - results[0].c - 1;//전체펀딩인원 - 펀딩참여인원 - 펀딩대표 = 남은 인원
-        // console.log(groups.people);
-        // console.log(results[0].c);
-        // console.log(people);
         res.render("funding/joinFunding", { group: groups, juso: juso, people: people });//res.locals.group에 [{키:값}]형태로 있어서 인덱스로 첫번째 객체 가져옴.
     },
     getJoinFundingClick: async (req, res) => { //펀딩 참여눌렀을 때 확인페이지
         try {
-            
-            // let currentUser = res.locals.currentUser;
             let userId = res.locals.currentUser.getDataValue('userId');
             let groups = res.locals.group[0];
             let juso = getJuso(groups);
@@ -201,12 +168,10 @@ module.exports = {
                          users.phoneNumber
                     FROM
                         users
-                    WHERE users.userId = ${userId};` //로그인한 유저의 정보로 수정필요함!!!!!!!
-                    //유저 id 로그인한 유저의 id로 수정필요 1 => currentUser.userId or currentUser.getDataValue('userId')
+                    WHERE users.userId = ${userId};` 
             let result = await sequelize.query(query, { type: Sequelize.SELECT });
             res.locals.user = result[0];
             let user = res.locals.user[0];
-            // let price = (groups.unit * groups.unitPrice) + (groups.deliveryCost / (groups.people - 1));
             let price = res.locals.price;
 
             let composition = await Composition.findOne({ //이미 참여한 펀딩인지 확인하기 위해
@@ -230,61 +195,19 @@ module.exports = {
         }
     },
     joinRequest: async (req, res, next) => {
-        // let data = getDataParams(req.body);
-        // let date = res.locals.group[0];
-        // let composition = await Composition.create(date.);
-
-        
-        // let currentUser = res.locals.currentUser;
 
         let groups = res.locals.group[0];
         let groupId = req.params.groupId
-        // let query = `SELECT  
-        //                  userId
-        //             FROM
-        //                 users
-        //             WHERE userId = 3;` //로그인한 유저의 정보로 수정필요함!!!!!!!
-        //              //유저 id 로그인한 유저의 id로 수정필요 1 => currentUser.userId or currentUser.getDataValue('userId')
-        // let [results, metadata] = await sequelize.query(query, { type: Sequelize.SELECT });
-        // let userId = results[0].userId;
-
-        // let composition = await Composition.findOne({ //이미 참여한 펀딩인지 확인하기 위해
-        //     where: {
-        //         fundingGroupId: groupId,
-        //         userId: userId
-        //     }
-        // });
-        // if (composition) {
-        //     //처음 펀딩목록화면으로 이동 후 플레시메시지
-
-        //     console.log("참여한펀딩");
-        //     req.flash('info', '이미 참여한 펀딩입니다.')
-        //     res.redirect('/joinfundingPage/fundingPage');
-        // } else {
-                // let query = `
-                //                 SELECT COUNT(*)
-                //                 FROM compositions
-                //                 WHERE compositions.fundingGroupId = 2;`
-                // let [results, metadata] = await sequelize.query(query, { type: Sequelize.SELECT });
-                // let peopleCount = results[0];
-
-            // if (peopleCount > 0) { //참여인원이 다 차지 않았다면
-            
+       
                 let userId = res.locals.currentUser.getDataValue('userId');
                 let price = res.locals.price;
                 let newComposition = await Composition.create({ //펀딩참여시 composition테이블에 추가
                     fundingGroupId: groupId,
-                    userId: userId, //로그인한 유저의 ID로 수정필요!!!!!!!!!
+                    userId: userId, 
                     quantity: groups.unit,
                     amount: price
                 });
-                // let people = groups.people
-                // let changePeople = await FundingGroup.update({ //펀딩참여인원 -1
-                //     people: people - 1
-                // }, { where: { fundingGroupId: groupId } });
                 next();
-            // }
-        // }
     },
     getJoinFundingComplete: async (req, res) => { //참여완료하고 알림?정보 보여주는 페이지
         res.render("funding/joinFundingComplete", { group: res.locals.group[0] });
