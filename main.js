e536f2c07e02377194b815275fe2efbf4b255331
nagaeng <helen030816@naver.com>
2024-12-06 17:15:50 +0000

    [mod] main.js port 번호 수정

--- main.js
+++ main.js
@@ -133,7 +133,7 @@ app.use("/joinfundingPage", joinFundingRouter);
 // app.use('/auth', authRouter);
 
 // 서버 실행
-app.set("port", 8080);
+app.set("port", 3000);
 app.listen(app.get("port"), "0.0.0.0", () => {
     console.log(`Server running at http://localhost:${app.get("port")}`);
 });
